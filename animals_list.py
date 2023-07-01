4138018fb4933da42ebb2f4e5eb9aca9453e5a72
Shakhemir <shakhemir@gmail.com>
2023-07-01 05:39:12 +0300

    15.Создайте класс Счетчик, у которого есть метод add(), увеличивающий̆ значение внутренней̆int переменной̆на 1 при нажатие “Завести новое животное” Сделайте так, чтобы с объектом такого типа можно было работать в блоке try-with-resources. Нужно бросить исключение, если работа с объектом типа счетчик была не в ресурсном try и/или ресурс остался открыт. Значение считать в ресурсе try, если при заведения животного заполнены все поля.

--- animals_list.py
+++ animals_list.py
@@ -11,34 +11,32 @@ def init_animal(name, date_of_birth, specie) -> Animal:
             return animal(name, date_of_birth)
     return pets.Cat(name, date_of_birth)
 
-    # match specie:
-    #     case 'cat':
-    #         new_animal = pets.Cat(name, date_of_birth)
-    #     case 'cow':
-    #         new_animal = pets.Cow(name, date_of_birth)
-    #     case 'dog':
-    #         new_animal = pets.Dog(name, date_of_birth)
-    #     case 'horse':
-    #         new_animal = pack_animals.Horse(name, date_of_birth)
-    #     case 'donkey':
-    #         new_animal = pack_animals.Donkey(name, date_of_birth)
-    #     case 'hamster':
-    #         new_animal = pets.Hamster(name, date_of_birth)
-    #     case _:
-    #         new_animal = pack_animals.Mule(name, date_of_birth)
-    # return new_animal
+
+class Counter:
+    def __init__(self):
+        self.count = 0
+
+    def inc(self):
+        # Вместо add назвал inc т.к. реализовано удаление животных тоже
+        # и такое название будет правильнее
+        self.count += 1
+
+    def dec(self):
+        self.count -= 1
 
 
 class AnimalList:
     def __init__(self):
         self.all_animals = []
         self.load_animals(db_io.load_all_data())
+        self.counter = Counter()
 
     def load_animals(self, fetched_list: list):
         for db_id, name, date_of_birth, commands, specie in fetched_list:
             new_animal = init_animal(name, date_of_birth, specie)
             new_animal.add_commands(commands.split(','))
             self.all_animals.append((db_id, new_animal))
+            self.counter.inc()
 
     def get_animal(self, animal_id) -> Animal:
         return self.all_animals[animal_id][1]
@@ -47,6 +45,7 @@ class AnimalList:
         new_animal = init_animal(name, date_of_birth, specie)
         db_id = db_io.add_new_animal(name, date_of_birth, specie)
         self.all_animals.append((db_id, new_animal))
+        self.counter.inc()
 
     def add_command(self, animal_id, commands):
         db_id, animal = self.all_animals[animal_id]
@@ -57,6 +56,7 @@ class AnimalList:
         db_id, animal = self.all_animals[animal_id]
         db_io.delete_animal(db_id)
         del self.all_animals[animal_id]
+        self.counter.dec()
 
     def show_all_animals(self):
         for _, animal in self.all_animals:
